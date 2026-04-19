5742ae66e527defebd14fce65be860d5fb292742
babathakur855 <44197769+babathakur855@users.noreply.github.com>
2026-04-20 00:10:49 +0530

    Update modules/control-tower/outputs.tf

--- modules/control-tower/outputs.tf
+++ modules/control-tower/outputs.tf
@@ -1,9 +1,25 @@
-output "landing_zone_id" {
-  description = "Control Tower Landing Zone ID"
-  value       = aws_controltower_landing_zone.this.id
+output "landing_zone_status" {
+  description = "Control Tower landing zone validation status."
+  value       = local.landing_zone_status
 }
 
-output "landing_zone_arn" {
-  description = "Control Tower Landing Zone ARN"
-  value       = aws_controltower_landing_zone.this.arn
+output "management_account_id" {
+  description = "Management account ID."
+  value       = data.aws_caller_identity.management.account_id
+}
+
+output "home_region" {
+  description = "Control Tower home region."
+  value       = var.home_region
+}
+
+output "ssm_parameter_arns" {
+  description = "ARNs of all SSM parameters created."
+  value = {
+    home_region        = aws_ssm_parameter.ct_home_region.arn
+    management_account = aws_ssm_parameter.ct_management_account_id.arn
+    log_archive        = aws_ssm_parameter.ct_log_archive_account_id.arn
+    audit              = aws_ssm_parameter.ct_audit_account_id.arn
+    status             = aws_ssm_parameter.ct_landing_zone_status.arn
+  }
 }
